c30be5a923655719d2caabd51d066f3fb4af00c6
NemoChen <nemo.chen@amazingtalker.com>
2021-06-25 01:26:36 +0800

    feature: Modify the push as force push

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git push -u origin HEAD:$OUTPUT_BRANCH
+  git push -uf origin HEAD:$OUTPUT_BRANCH
 else
   echo "No changes detected"
 fi
