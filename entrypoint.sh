034779a75a258454b40c5d97905b5f68bde3987e
kuanhuachen <kuanhua.chen@amazingtalker.com>
2022-06-02 17:27:05 +0800

    add run script

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,18 @@ git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-t
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+
+echo $(pwd)
+
+cd ..
+cd ..
+cd "$INPUT_SCRIPT_FILE_DESTINATION"
+ruby "$INPUT_SCRIPT_FILE".rb
+
+echo $(pwd)
+
 cd "$CLONE_DIR"
+
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
